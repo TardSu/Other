fdf27966f14403f8885303426ad19651200c1873
TardSu <everyday.tard@hotmail.com>
2017-08-21 20:18:08 +0700

    Add files via upload

---
+++ [VisualStudio2017]GEN_SSD1306/GEN_SSD1306/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace GEN_SSD1306
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripLabel2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripLabel4 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripLabel3 = new System.Windows.Forms.ToolStripButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(11, 28);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(256, 512);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1,
+            this.toolStripLabel2,
+            this.toolStripLabel4,
+            this.toolStripLabel3});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(279, 25);
+            this.toolStrip1.TabIndex = 3;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(0, 22);
+            // 
+            // toolStripLabel2
+            // 
+            this.toolStripLabel2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripLabel2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripLabel2.Image")));
+            this.toolStripLabel2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripLabel2.Name = "toolStripLabel2";
+            this.toolStripLabel2.Size = new System.Drawing.Size(37, 22);
+            this.toolStripLabel2.Text = "Load";
+            this.toolStripLabel2.Click += new System.EventHandler(this.toolStripLabel2_Click);
+            // 
+            // toolStripLabel4
+            // 
+            this.toolStripLabel4.Name = "toolStripLabel4";
+            this.toolStripLabel4.Size = new System.Drawing.Size(31, 22);
+            this.toolStripLabel4.Text = "        ";
+            // 
+            // toolStripLabel3
+            // 
+            this.toolStripLabel3.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripLabel3.Enabled = false;
+            this.toolStripLabel3.Image = ((System.Drawing.Image)(resources.GetObject("toolStripLabel3.Image")));
+            this.toolStripLabel3.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripLabel3.Name = "toolStripLabel3";
+            this.toolStripLabel3.Size = new System.Drawing.Size(58, 22);
+            this.toolStripLabel3.Text = "Generate";
+            this.toolStripLabel3.Click += new System.EventHandler(this.toolStripLabel3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(279, 551);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripButton toolStripLabel2;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel4;
+        private System.Windows.Forms.ToolStripButton toolStripLabel3;
+    }
+}
+
---
+++ [VisualStudio2017]GEN_SSD1306/GEN_SSD1306/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows;
+using System.IO;
+
+namespace GEN_SSD1306
+{
+    public partial class Form1 : Form
+    {
+        Bitmap pic;
+        private int color8 = 128;
+        private char[] hex_table = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+
+        string file_full_path;
+        string file_path;
+        string file_name;
+
+        int pic_Height;
+        int pic_Width;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string byte2hex(byte a)
+        {
+            string hex = "0x";
+            hex = hex + hex_table[(byte)(a / 16)];
+            hex = hex + hex_table[(byte)(a % 16)];
+            return hex;
+        }
+
+        private bool check_black_pixel(Color c)
+        {
+            int r = c.R;
+            int g = c.G;
+            int b = c.B;
+            if (System.Math.Sqrt(r * r + g * g + b * b) < color8)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+
+        private void toolStripLabel2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = "Image Files(*.bmp)|*.bmp";
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                pic = new Bitmap(open.FileName);
+                pictureBox1.Image = pic;
+
+                pic_Height = pic.Height;
+                pic_Width = pic.Width;
+
+                file_full_path = open.FileName;
+                file_name = Path.GetFileName(file_full_path);
+                file_path = Path.GetDirectoryName(file_full_path);
+
+                int dot;
+                string file_ext = ".bmp";
+                string file_search = file_name.ToUpper();
+
+                dot = file_search.IndexOf(file_ext.ToUpper());
+
+                file_name = file_search.Substring(0, dot).ToLower();
+                toolStripLabel3.Enabled = true;
+
+            }
+        }
+
+        private void toolStripLabel3_Click(object sender, EventArgs e)
+        {
+            byte x;
+            byte code;
+            int i, j, k;
+
+            /*Write the string to a file.*/
+            System.IO.StreamWriter file = new System.IO.StreamWriter(file_path + "\\" + file_name + ".h");
+
+            file.WriteLine("const unsigned char " + file_name + "[] =");
+            file.Write("{");
+
+            for (j = 0; j < pic_Height; j += 8)
+            {
+                for (i = 0; i < pic_Width; i++)
+                {
+                    x = 0x01;
+                    code = 0;
+                    for (k = 0; k < 8; k++)
+                    {
+                        if (check_black_pixel(pic.GetPixel(i, j + k)))
+                        {
+                            code += x;
+                        }
+                        x = (byte)(x * 2);
+                    }
+
+                    if (i % 8 == 0)
+                        file.Write("\r\n    ");
+
+                    file.Write(byte2hex(code) + ",");
+                }
+            }
+            file.WriteLine("\r\n};");
+            file.Close();
+
+            MessageBox.Show("Generate complete.");
+        }
+    }
+}
